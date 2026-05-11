8931db754ccdca2cadf7aaeb564eacff7006fac1
gas7119 <184426179+gas7119@users.noreply.github.com>
2026-05-11 12:33:12 -0300

    a

---
+++ Atividade13/js.js
@@ -0,0 +1,9 @@
+const links = ["https://thumbs.dreamstime.com/z/%C3%ADcone-do-sensor-de-quebra-vidro-isolado-em-fundo-branco-logotipo-vetorial-simples-254988558.jpg?ct=jpeg", "https://thumbs.dreamstime.com/z/%C3%ADcone-de-glifo-do-sensor-quebra-vidro-isolado-em-fundo-branco-logotipo-vetorial-simples-221190224.jpg?ct=jpeg", "https://thumbs.dreamstime.com/z/%C3%ADcone-de-janela-quebrada-isolado-em-fundo-branco-logotipo-vetorial-simples-221193222.jpg"]
+const descs = ["Aberta", "Fechada", "Quebrada"]
+
+function Janela(n) {
+	if (document.getElementById("t").innerText != "Janela Quebrada") {
+		document.getElementById("t").innerText = "Janela " + descs[n];
+		document.getElementById("j").src = links[n];
+	}
+}

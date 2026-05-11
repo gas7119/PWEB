289ca2557dac374ad842540b40e038934b5de825
gas7119 <184426179+gas7119@users.noreply.github.com>
2026-05-11 12:39:11 -0300

    Add new window state description and update logic

--- Atividade13/js.js
+++ Atividade13/js.js
@@ -1,9 +1,9 @@
 const links = ["https://thumbs.dreamstime.com/z/%C3%ADcone-do-sensor-de-quebra-vidro-isolado-em-fundo-branco-logotipo-vetorial-simples-254988558.jpg?ct=jpeg", "https://thumbs.dreamstime.com/z/%C3%ADcone-de-glifo-do-sensor-quebra-vidro-isolado-em-fundo-branco-logotipo-vetorial-simples-221190224.jpg?ct=jpeg", "https://thumbs.dreamstime.com/z/%C3%ADcone-de-janela-quebrada-isolado-em-fundo-branco-logotipo-vetorial-simples-221193222.jpg"]
-const descs = ["Aberta", "Fechada", "Quebrada"]
+const descs = ["Aberta", "Fechada", "Quebrada", "Abra a Janela"]
 
 function Janela(n) {
 	if (document.getElementById("t").innerText != "Janela Quebrada") {
-		document.getElementById("t").innerText = "Janela " + descs[n];
-		document.getElementById("j").src = links[n];
+		document.getElementById("t").innerText = (n != 3 ? "Janela " : "") + descs[n];
+		document.getElementById("j").src = n == 3 ? links[1] : links[n];
 	}
 }
